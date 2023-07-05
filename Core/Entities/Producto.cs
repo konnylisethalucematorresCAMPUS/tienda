77272bdf02c99c7739f8f250d5e5f25ea67c70c5
konny alucema <lisethtorres969@gmail.com>
2023-07-05 09:22:04 -0500

    EMPEZANDO A VIVIR EN ESTE MUNDO CRUEL

---
+++ Core/Entities/Producto.cs
@@ -0,0 +1,8 @@
+namespace Core.Entities;
+
+public class Producto{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public DateTime FechaCreacion { get; set; }
+}
